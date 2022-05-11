1951c445646a1b913cfadfb18edec56290730eec
Andrey Okatev <okatev87@yandex.ru>
2022-05-11 23:41:15 +0300

    создал логику проверки имени

---
+++ src/main/java/Account.java
@@ -0,0 +1,28 @@
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+
+    public boolean checkNameToEmboss() {
+        if (name.length() >= 19 || name.length() < 3) {
+            return false;
+        }
+        if (!name.trim().contains(" ")) {
+            return false;
+        }
+        if (name.charAt(0) == ' ' || name.charAt(name.length() - 1) == ' ') {
+            return false;
+        }
+        if (name.trim().split(" ").length > 2)
+            return false;
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,7 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        Account account = new Account("");
+        account.checkNameToEmboss();
+        System.out.println(account.checkNameToEmboss());
+    }
+}
